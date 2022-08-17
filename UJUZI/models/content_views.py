44abea695d4675f253959faf19dd13593fe63917
ranboy12 <frmsonge@mzumbe.ac.tz>
2022-08-17 16:24:10 +0300

    database changes 648

--- UJUZI/models/content_views.py
+++ UJUZI/models/content_views.py
@@ -2,13 +2,12 @@ import datetime
 from django.db import models
 from django.conf import settings
 
-from ..models import Module
 
 User = settings.AUTH_USER_MODEL
 
 
 class ContentViewers(models.Model):
-    content = models.ForeignKey(Module, on_delete=models.CASCADE)
+    content = models.ForeignKey('Module', on_delete=models.CASCADE)
     viewer = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.date.today, editable=False)
 
@@ -21,15 +20,3 @@ class ContentViewers(models.Model):
         return self.content
 
 
-class TotalContentViewers(models.Model):
-    content = models.ForeignKey(Module, on_delete=models.CASCADE, unique=True)
-    total = models.IntegerField(default=0)
-    date = models.DateField(default=datetime.date.today, editable=False)
-
-    class Meta:
-        unique_together = ('content', 'viewer', 'date')
-        verbose_name = "Total Content Viewers"
-        verbose_name_plural = "Total Content Viewers"
-
-    def __str__(self):
-        return self.content
