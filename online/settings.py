3a1f151f106355c23ac3e1cd8e96bac660cb00dd
luggiestar <luggiestar@gmail.com>
2022-08-19 13:43:18 +0300

    luggie

--- online/settings.py
+++ online/settings.py
@@ -176,7 +176,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT is for server path to store files in computer
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media-root/')
 
 
 # Default primary key field type
